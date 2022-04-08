ba079e4e82cc579a44b6859326c1797f3fd133cf
Ruan Pablo <ruan_goio10@hotmail.com>
2022-04-08 12:01:48 -0300

    :scroll: Profile screen suit

--- src/__tests__/screens/perfil.test.tsx
+++ src/__tests__/screens/perfil.test.tsx
@@ -2,20 +2,36 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { Perfil } from '../../../src/screens/Perfil';
 
-test('Check if exist the input with placeholder Nome', () => {
-    const { getByPlaceholderText } = render(<Perfil />);
-    const inputNome = getByPlaceholderText('Nome');
-    expect(inputNome).toBeTruthy();
-});
+describe('Profile Screen', () => {
+    it('Check if exist the input with placeholder Nome', () => {
+        const { getByPlaceholderText } = render(<Perfil />);
+        const inputNome = getByPlaceholderText('Nome');
+        expect(inputNome).toBeTruthy();
+    });
 
-test('Check if exist the input with placeholder Sobrenome', () => {
-    const { getByPlaceholderText } = render(<Perfil />);
-    const inputSobrenome = getByPlaceholderText('Sobrenome');
-    expect(inputSobrenome).toBeTruthy();
-});
+    it('Check if exist the input with placeholder Sobrenome', () => {
+        const { getByPlaceholderText } = render(<Perfil />);
+        const inputSobrenome = getByPlaceholderText('Sobrenome');
+        expect(inputSobrenome).toBeTruthy();
+    });
 
-test('Check if exist the button with title Salvar', () => {
-    const { getByText } = render(<Perfil />);
-    const buttonSalvar = getByText('Salvar');
-    expect(buttonSalvar).toBeTruthy();
+    it('Check if exist the button with title Salvar', () => {
+        const { getByText } = render(<Perfil />);
+        const buttonSalvar = getByText('Salvar');
+        expect(buttonSalvar).toBeTruthy();
+    });
+
+    it('Check if user data as been loaded', () => {
+        const { getByTestId } = render(<Perfil />);
+        const inputName = getByTestId('input-name');
+        const inputLastName = getByTestId('input-lastName');
+        expect(inputName.props.value).toBe('Ruan Pablo');
+        expect(inputLastName.props.value).toBe('Gomes Rocha');
+    });
+
+    it('Check if the title is correct', () => {
+        const { getByTestId } = render(<Perfil />);
+        const pageTitle = getByTestId('page-title');
+        expect(pageTitle.props.children).toBe('Perfil');
+    });
 });
--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -14,19 +14,24 @@ export function Perfil() {
 
     return (
         <View>
-            <Text>Perfil</Text>
+            <Text testID='page-title'>Perfil</Text>
 
             <TextInput
+                testID='input-name'
                 placeholder='Nome'
                 autoCorrect={false}
+                value={'Ruan Pablo'}
             />
 
             <TextInput
+                testID='input-lastName'
                 placeholder='Sobrenome'
                 autoCorrect={false}
+                value={'Gomes Rocha'}
             />
 
             <Button
+                testID='save-button'
                 title='Salvar'
                 onPress={sayHello}
             />
